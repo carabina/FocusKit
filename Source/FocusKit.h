a411b9c40c518c4e5ae15374187933515d546fb7
Daniel Dahan <daniel@graphkit.io>
2015-10-26 13:33:45 -0400

    master: updated

--- Source/FocusKit.h
+++ Source/FocusKit.h
@@ -16,31 +16,12 @@
 // in a file called LICENSE.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-import XCTest
-@testable import FocusKit
+#import <UIKit/UIKit.h>
 
-class FocusKitTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
-}
+//! Project version number for Temp.
+FOUNDATION_EXPORT double TempVersionNumber;
+
+//! Project version string for Temp.
+FOUNDATION_EXPORT const unsigned char TempVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Temp/PublicHeader.h>
